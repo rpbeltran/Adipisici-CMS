fab3f6ccce5994532f536f3bf48d5026e156b061
rpbeltran <rp.beltran@yahoo.com>
2017-03-30 00:04:54 -0500

    Refactored data model. Pushed main dependency to Mongoose.

--- examples/index.js
+++ examples/index.js
@@ -1,36 +1,47 @@
 
 // Require Adipisici
-Adipisici = require( './lib/adipisici' );
+Adipisici = require( '../lib/adipisici.js' );
 
 // Create and initialize Adipisici server
 website = new Adipisici( "My Website", "mongodb://localhost:27017/mywebsite", 8080 );
 
 // Data Modeling
-website.database.initiate_connection().then( function( ) {
+website.connect_database().then( function( ) {
 
-    website.createDataForm( 'blog_post',
-        {
+    website.createDataForm( 'blog_post', {
 
-            name : {
-                required: true
-            },
+        name : {
+            type: String,
+            required: true
+        },
 
-            url : {
-                required: true,
-                unique: true
-            },
+        url : {git
+            type: String,
+            required: true,
+            unique: true
+        },
 
+        cover : {
+            type: String,
+            required: false
+        },
 
-            cover : {
-                required: false
-            },
+        summary : {
+            type : String,
+            required: false
+        },
 
-            summary : {
-            	required: false
-            }
+        reads : {
+            type : Number,
+            default: 0
+        },
 
+        content : {
+            type: String,
+            default: "Lorem ipsum dolor sit amet."
         }
-    );
+
+    } );
 
 }).catch( function( error ) {
     console.log( error );
@@ -41,27 +52,25 @@ website.database.initiate_connection().then( function( ) {
 
 // Retrieve all blog posts
 website.server.createEndpoint( 'get', '/blog', function(request ) {
-    return website.database.allDocuments( 'blog_post' );
+    return website.data_layer.all( 'blog_post' );
 } );
 
 // Retrieve a specific blog post
 website.server.createEndpoint( 'get', '/blog/:blog_url', function(request ) {
-    return website.database.find( 'blog_post', { url : request.params.blog_url } );
+    return website.data_layer.find( 'blog_post', { url : request.params.blog_url } );
 } );
 
 // Upsert blog post
 website.server.createEndpoint( 'put', '/blog/:blog_url', function(request ) {
-    return website.format( 'blog_post', request.body ).then( function( entry ) {
-        website.database.upsert( 'blog_post', { url : request.params.blog_url }, entry );
-    } ) ;
+    return website.data_layer.upsert( 'blog_post', { url : request.params.blog_url }, request.params );
 } );
 
 // Edit blog post
 website.server.createEndpoint( 'patch', '/blog/:blog_url', function(request ) {
-    return website.database.update( 'blog_post', { url : request.params.blog_url }, request.body );
+    return website.data_layer.update( 'blog_post', { url : request.params.blog_url }, request.body );
 } );
 
 // Delete blog post
 website.server.createEndpoint( 'delete', '/blog/:blog_url', function(request ) {
-    return website.database.delete( 'blog_post', { url : request.params.blog_url } );
+    return website.data_layer.delete( 'blog_post', { url : request.params.blog_url } );
 } );
