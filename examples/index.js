183cce008ca9480434d165f7a65c2bfa257360fa
rpbeltran <rp.beltran@yahoo.com>
2017-06-24 21:28:08 -0500

    Added settings and refactored endpoints to add support for hooks

--- examples/index.js
+++ examples/index.js
@@ -1,12 +1,19 @@
 
+
 // Require Adipisici
 Adipisici = require( '../lib/adipisici.js' );
 
 // Create and initialize Adipisici server
-website = new Adipisici( "My Website", "mongodb://localhost:27017/mywebsite", 8080 );
+website = new Adipisici( "My Website" );
+
+website.startListening().then( () => {
+    console.log( 'My Website is listening on port ' + website.server.settings.port );
+});
 
 // Data Modeling
-website.connect_database().then( function( ) {
+website.connect_database().then( ( ) => {
+
+    console.log( 'My website is connected to a mongo database at ' + website.mongo_url );
 
     website.createDataForm( 'blog_post', {
 
@@ -38,7 +45,7 @@ website.connect_database().then( function( ) {
 
     } );
 
-}).catch( function( error ) {
+}).catch( ( error ) => {
     console.log( error );
 });
 
@@ -46,26 +53,26 @@ website.connect_database().then( function( ) {
 // Blog Post Endpoints
 
 // Retrieve all blog posts
-website.server.createEndpoint( 'get', '/blog', function( request ) {
+website.server.createEndpoint( 'get', '/blog', ( request ) => {
     return website.data_layer.all( 'blog_post' );
 } );
 
 // Retrieve a specific blog post
-website.server.createEndpoint( 'get', '/blog/:blog_url', function(request ) {
+website.server.createEndpoint( 'get', '/blog/:blog_url', ( request ) => {
     return website.data_layer.find( 'blog_post', { url : request.params.blog_url } );
 } );
 
 // Upsert blog post
-website.server.createEndpoint( 'put', '/blog/:blog_url', function(request ) {
+website.server.createEndpoint( 'put', '/blog/:blog_url', ( request ) => {
     return website.data_layer.upsert( 'blog_post', { url : request.params.blog_url }, request.body );
 } );
 
 // Edit blog post
-website.server.createEndpoint( 'patch', '/blog/:blog_url', function(request ) {
+website.server.createEndpoint( 'patch', '/blog/:blog_url', ( request ) => {
     return website.data_layer.update( 'blog_post', { url : request.params.blog_url }, request.body );
 } );
 
 // Delete blog post
-website.server.createEndpoint( 'delete', '/blog/:blog_url', function(request ) {
+website.server.createEndpoint( 'delete', '/blog/:blog_url', ( request ) => {
     return website.data_layer.delete( 'blog_post', { url : request.params.blog_url } );
 } );
